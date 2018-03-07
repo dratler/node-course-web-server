779ac9b7ad324663d8325a16fa2ccecfb7756273
dratler <dratler.shay@gmail.com>
2018-03-07 23:30:15 +0200

    setting start on package.json

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const hbs = require('hbs');
 const app = express();
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
@@ -49,4 +50,4 @@ app.get("/bad", (req, res) => {
 });
 
 
-app.listen(3000, () => {console.log('Server is on port 3000');});
+app.listen(port, () => {console.log(`Server is on port ${port}`);});
